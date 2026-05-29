3b085f8c2754a9ff6f5591ad0ee52040eeb80e6f
Lari_Visioli <larissa.visioli@edu.senai.br>
2026-05-29 10:51:45 -0300

    Mudança do combustível

--- Controle_da_Nave.py
+++ Controle_da_Nave.py
@@ -1,6 +1,6 @@
 # definir variáveis
 
-combustivel = 100
+combustivel = 110
 tripulantes = []
 
 ##Definir funções
@@ -16,7 +16,7 @@ def viajar():
 
 def abastecer():
     global combustivel
-    combustivel = 100
+    combustivel = 110
     print("Tanque cheio!⛽")
 
 def status_nave():
