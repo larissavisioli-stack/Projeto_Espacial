c7598a13e24863aab5df8d7f9d01a7154c2a77b4
Lari_Visioli <larissa.visioli@edu.senai.br>
2026-06-11 09:47:54 -0300

    Criei a função de remover um tripulante

--- Controle_da_Nave.py
+++ Controle_da_Nave.py
@@ -31,12 +31,22 @@ def registrarTripulante():
     novoTripulante = input("Qual o nome do novo tripulante?:")
     tripulantes.append(novoTripulante)
     print("Tripuante inserido com sucesso!🚀")
+def removerTripulante():
+     global tripulantes
+
+     if len(tripulantes) == 0:
+         print("Não há tripulantes para serem removidos")
+
+     else:
+          tripulantes.pop()
+          print(f"Tripulante removido com sucesso! os tripulantes restantes são: {tripulantes}")
+
 
 ### Criar um menu
 
 while True: ##esse loop roda para sempre
     print("Bem-vindo ao menu interativo da nave. Por favor selecione uma opção")
-    print("\n 1- Mostrar status da nave | 2- Viajar | 3- Abastecer | 4 - Novo Tripulante | 5 Sair ")
+    print("\n 1- Mostrar status da nave | 2- Viajar | 3- Abastecer | 4- Novo Tripulante | 5- Sair | 6- Remover Tripulante")
     opcao = input("Escolha:")
 
     if (opcao == "1"):
@@ -52,6 +62,11 @@ while True: ##esse loop roda para sempre
     elif (opcao == "5"):
         print("Viagem encerrada!")
         break
+
+    elif (opcao == "6"):
+        removerTripulante()
+
+
 
 
 #status_nave()
